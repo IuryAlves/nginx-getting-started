8725eb6a2b433551e74ab2d469817a97adcdb197
IuryAlves <satriani-16@hotmail.com>
2017-06-28 16:45:45 -0300

    splitting environments by ENV

--- app_server/app.py
+++ app_server/app.py
@@ -4,16 +4,14 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-port = os.getenv('PORT')
 
 
 @app.route("/")
 def hello():
-    if port == '8001':
-        return "Hello from Application Server A"
-    elif port == '8002':
-        return "Hello from Application Server B"
+    env = os.getenv('ENV')
+    return "Hello from Application Server {0}".format(env)
 
 
 if __name__ == "__main__":
+    port = os.getenv('PORT')
     app.run("0.0.0.0", port=port)
